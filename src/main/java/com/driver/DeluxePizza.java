11367fbdfaaf4958b5f35f43521745a6ae6c00d7
Ananya-Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-01-21 11:05:09 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,6 +5,8 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        private Boolean isCheeseAddedOnce;
+        private Boolean isToppingsAddedOnce;
         super(extraCheese);
         super(extraToppings);
     }
