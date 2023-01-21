80c4304e2ed47755855b683fdb119fb5059a030c
Ananya-Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-01-21 10:54:18 +0530

    Update Pizza.java

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,41 +5,65 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private Boolean isCheeseAddedOnce;
+    private Boolean isToppingsAddedOnce;
+    private Boolean isTakeAwayAddedOnce;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
         if(isVeg==true)
+        {
             price+=300;
+            bill=bill+"Base Price Of The Pizza: 300\n";
+        }
         else
+        {
             price+=400;
+            bill=bill+"Base Price Of The Pizza: 400\n";
+        }
     }
 
     public int getPrice(){
         return this.price;
     }
 
-    public void addExtraCheese(){
+    public void addExtraCheese(Boolean isCheeseAddedOnce){
         // your code goes here
-        price+==80;
+        this.isCheeseAddedOnce=isCheeseAddedOnce;
+        if(isCheeseAddedOnce==false)
+        {
+            price+=80;
+            isCheeseAddedOnce=true;
+        }
+        
     }
 
-    public void addExtraToppings(){
+    public void addExtraToppings(Boolean isToppingsAddedOnce){
         // your code goes here
-        if(isVeg==true)
-            price+=70;
-        else
-            price+=120;
+        this.isToppingsAddedOnce=isToppingsAddedOnce;
+        if(isToppingsAddedOnce==false)
+        {
+            if(this.isVeg==true)
+                price+=70;
+            else
+                price+=120;
+            isToppingsAddedOnce=true;
+        }
     }
 
-    public void addTakeaway(){
+    public void addTakeaway(Boolean isTakeAwayAddedOnce){
         // your code goes here
-        price+=20;
+        this.isTakeAwayAddedOnce=isTakeAwayAddedOnce;
+        if(isTakeAwayAddedOnce==false)
+        {
+            price+=20;
+            isTakeAwayAddedOnce=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
         return this.bill;
-        return this.price;
     }
 }
